fdb0b08ea498646c8cc260d316a9eca856e02e56
Arian Mirahmadi <mirarianmir@gmail.com>
2021-07-02 13:58:20 +0430

    feat: run python after compile

--- language.rs
+++ language.rs
@@ -1,17 +1,28 @@
 use std::env;
 use std::fs;
 use std::io::Write;
+use std::process::Command;
+use std::process::Output;
 
 
 fn main() {
-    let (content, destination) = get_and_read_inputs();
+    let (content, destination, should_run) = get_and_read_inputs();
 
     let result = write_file(&destination, &content);
-    print_result(result, destination);
+    print_result(result, &destination);
+
+    if should_run {
+        let output: Output = run_file(&destination);
+        let stdout = match std::str::from_utf8(&output.stdout) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        println!("Output: \n{}", stdout)
+    }
 }
 
 
-fn get_and_read_inputs() -> (String, String) {
+fn get_and_read_inputs() -> (String, String, bool) {
     let args = get_env_args();
 
     if args.len() < 2 {
@@ -28,7 +39,11 @@ fn get_and_read_inputs() -> (String, String) {
         destination = args[2].to_owned();
     }
 
-    ( content, destination )
+    let should_run: bool = args.iter().any(
+        |arg| arg=="-r"
+    );
+
+    ( content, destination, should_run )
 }
 
 
@@ -50,9 +65,28 @@ fn write_file(destination: &String, content: &String) -> std::io::Result<()> {
 }
 
 
-fn print_result(result: std::io::Result<()>, destination: String) {
+fn print_result(result: std::io::Result<()>, destination: &String) {
     match result {
         Ok(_) => println!("Successfully compiled at '{}'",destination),
         Err(err) => println!("\n\tFaild Compiling\n\t{}\n", err),
     }
+}
+
+fn run_file(destination: &String) -> Output {
+    let mut command: String = "py ".to_owned();
+    command.push_str(&destination);
+    command.push_str(".py");
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(&["/C", &command])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg(&command)
+                .output()
+                .expect("failed to execute process")
+    };
+    output
 }
