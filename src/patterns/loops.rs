4b831367c19f399af39b639c8cde5cb9c01be59f
Arian Mirahmadi <mirarianmir@gmail.com>
2021-07-09 11:58:43 +0430

    fix: line break issue and feat: function defination with parameters

--- src/patterns/loops.rs
+++ src/patterns/loops.rs
@@ -1,7 +1,7 @@
 pub fn statements() -> Vec<[&'static str; 2]> {
     vec!(
-        [r"untill (?P<t>.+)", "while not $t:\t"],
-        [r"loop in (?P<t>.+)", "for index in $t:\t"],
+        [r"untill (?P<t>.+)\r\n", "while not $t:\n\t"],
+        [r"loop in (?P<t>.+)\r\n", "for index in $t:\n\t"],
         [r"do (?P<t>.+) forever", "while True:\n\t$t"],
     )
 }
