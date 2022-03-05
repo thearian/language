7c232c32e1acdc981698f9a0054b3561393fcd30
Arian Mirahmadi <mirarianmir@gmail.com>
2022-03-05 20:57:42 +0330

    fix: remove dir and files

--- src/patterns/functions.rs
+++
@@ -1,8 +0,0 @@
-pub fn statements() -> Vec<[&'static str; 2]> {
-    vec!(
-        [r"remember that (?P<n>\w+) is (?P<d>.+) and gets (?P<p>.+)\r\n","def $n($p):\n\t$d"],
-        [r"remember that (?P<n>\w+) is (?P<d>.+)","def $n():\n\t$d"],
-        [r"get (?P<n>.+)\r\n", "input($n)\n"],
-        [r" (?P<a>.\w+) to (?P<b>\w+)"," range($a,$b)"],
-    )
-}
--- src/patterns/loops.rs
+++
@@ -1,7 +0,0 @@
-pub fn statements() -> Vec<[&'static str; 2]> {
-    vec!(
-        [r"untill (?P<t>.+)\r\n", "while not $t:\n\t"],
-        [r"loop in (?P<t>.+)\r\n", "for index in $t:\n\t"],
-        [r"do (?P<t>.+) forever", "while True:\n\t$t"],
-    )
-}
